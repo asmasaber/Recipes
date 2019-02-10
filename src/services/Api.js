b3ee990dea5531d5838482ca8fbe5b5a63a43417
asmasaber <asmaa.saber93@gmail.com>
2019-02-10 18:46:39 +0200

    base url

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,8 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:8081',
+    // baseURL: 'http://localhost:8081',
+    baseURL: 'https://foodrecipesapp.herokuapp.com/',
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
